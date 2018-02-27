6c06ec14f4169f68f6f0d0f4a362f7432a9365c4
bkorinnyi <bohdan.korinnyi@easypark.net>
2018-02-27 11:52:38 +0200

    Init project

---
+++ src/main/java/com/arloid/alarmcall/AlarmCallApplication.java
@@ -0,0 +1,12 @@
+package com.arloid.alarmcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmCallApplication.class, args);
+	}
+}
---
+++ src/test/java/com/arloid/alarmcall/AlarmCallApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arloid.alarmcall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlarmCallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
