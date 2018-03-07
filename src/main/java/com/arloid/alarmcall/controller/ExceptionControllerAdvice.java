e8fc4cfff1b6b85f9228e8c5e3163616805b55b1
Vasily Karpushchenko <raverone@mail.ru>
2018-03-07 08:54:45 +0300

    Added exception controller advice

---
+++ src/main/java/com/arloid/alarmcall/controller/ExceptionControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.arloid.alarmcall.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.arloid.alarmcall.exception.SaveExistingEntityException;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(SaveExistingEntityException.class)
+    public void handleSaveExistingEntityException(SaveExistingEntityException ex, HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value());
+    }
+
+}
